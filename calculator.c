2b2a678e8c27ccdfc78635cd499218cd50d9a09e
Ashar Siddiqui <ashar.siddiqui1@gmail.com>
2019-10-19 19:21:22 +0530

    diff feature

--- calculator.c
+++ calculator.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 
-int sum(int a, int b){
-
-	return a+b;
-}
-
-
 int main(){
 
 	int a=5, b=10;
 	printf("sum = %d",sum(a,b));
 	return 0;
+}
+
+int diff(int a, int b){
+	return a-b;
 }
